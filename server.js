bb5fc28757a70567a50f210741d54d9e60042f75
Corey Cohen <ccohen_1@hotmail.com>
2018-10-04 19:01:19 -0400

    Added song request

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ var port = 4000;
 
 var server = http.createServer(function (req, res) { // Callback function
     // Response header
-    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.writeHead(200, { "Content-Type": "application/json" });
     // send response
     res.end("Hello World\n");
+
+    var song = {
+    	id: 12345,
+    	favorite: false,
+    	title: "Hello World",
+    	artist: "Node programmer",
+    	album: "Node EP"
+    };
+    res.end(JSON.stringify(song));
 });
 
 server.listen(port, function () { // Callback function
